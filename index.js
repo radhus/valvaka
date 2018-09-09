9cb26c75f55d6380db133476724555643e32e077
Zn4rK <alexander@paxxmedia.se>
2018-09-09 21:10:20 +0200

    Parallelize

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const { WebClient } = require('@slack/client');
+const {WebClient} = require('@slack/client');
 const slack = new WebClient(process.env.SLACK_TOKEN);
 const channelNames = process.env.CHANNEL_NAMES;
 
@@ -22,13 +22,10 @@ const checks = [
   },
 ];
 
-checks.forEach((check) => {
-  (async () => {
-    try {
-      const browser = await puppeteer.launch();
-
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 2 });
+puppeteer.launch().then(async (browser) => {
+  const promises = checks.map(
+    (check) => browser.newPage().then(async (page) => {
+      await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
 
       await page.goto(check.url, {timeout: 300000});
       const overlay = await page.$(check.selector);
@@ -36,14 +33,15 @@ checks.forEach((check) => {
 
       const dt = new Date();
       const name = check.name + " " + dt.toLocaleDateString() + " " + dt.toLocaleTimeString();
+
       await slack.files.upload({
         filename: name,
         channels: channelNames,
         file: screenshot,
       });
-      await browser.close();
-    } catch (e) {
-      console.error(e)
-    }
-  })()
+    }).catch(error => console.error)
+  );
+
+  await Promise.all(promises);
+  await browser.close();
 });
