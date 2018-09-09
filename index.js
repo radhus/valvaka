6dcc9e427661d56ab8a557cc9c7b0009ce9b42c5
William Johansson <radar@radhuset.org>
2018-09-09 20:31:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const { WebClient } = require('@slack/client');
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+const checks = [
+  {
+    name: "Riksdag",
+    url: "https://data.val.se/val/val2018/valnatt/R/rike/index.html",
+    selector: "#sida > div:nth-child(4) > table",
+  },
+  {
+    name: "Göteborg",
+    url: "https://data.val.se/val/val2018/valnatt/K/kommun/14/80/index.html",
+    selector: "#sida > div:nth-child(4) > table",
+  },
+];
+
+
+checks.forEach((check) => {
+  (async () => {
+    var browser = await puppeteer.launch();
+
+    var page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 2 });
+
+    await page.goto(check.url);
+    var overlay = await page.$(check.selector);
+    var screenshot = await overlay.screenshot();
+
+    var dt = new Date()
+    var name = check.name + " " + dt.toLocaleDateString() + " " + dt.toLocaleTimeString();
+    slack.files.upload({
+      filename: name,
+      channels: "#radhuset",
+      file: screenshot,
+    }).then((res) => {
+      console.log(res);
+    }).catch(console.error);
+
+    await browser.close();
+
+  })()
+})
